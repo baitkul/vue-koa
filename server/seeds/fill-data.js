c07be08f91a5e44f16acdcd57ff0d2838c214d99
bakir <bakir.aitkul@gmail.com>
2018-05-15 08:25:13 +0600

    Add add generating method

--- server/seeds/fill-data.js
+++ server/seeds/fill-data.js
@@ -1,3 +1,22 @@
-const data = []
+const faker = require('faker')
+const moment = require('moment')
 
-exports.seed = knex => knex.table('books').insert(data)
+function generateBooks() {
+  const data = []
+  for (let i = 0; i < 10000; i += 1) {
+    const book = {}
+    book.title = faker.company.bs()
+    book.author = faker.fake('{{name.lastName}} {{name.firstName}}')
+    book.description = faker.hacker.phrase()
+    book.date = moment(faker.date.past()).format('YYYY-MM-DD')
+
+    data.push(book)
+  }
+
+  return data
+}
+
+exports.seed = (knex) => {
+  const books = generateBooks()
+  return knex.table('books').insert(books)
+}
